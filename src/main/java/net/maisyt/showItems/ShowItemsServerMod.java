561beebac6c2118288b2d8e017fdc70a11d9eca0
tcm9439 <tsechingmaisy@gmail.com>
2023-07-03 22:47:11 +0800

    Fix multiplayer
    Fix the issue of other's item being show when in multiplayer mode.

--- src/main/java/net/maisyt/showItems/ShowItemsServerMod.java
+++ src/main/java/net/maisyt/showItems/ShowItemsServerMod.java
@@ -2,9 +2,13 @@ package net.maisyt.showItems;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
 import net.maisyt.showItems.command.GenerateDefaultConfigCommand;
 import net.maisyt.showItems.command.ReloadCommand;
 import net.maisyt.showItems.command.ShutdownCommand;
+import net.maisyt.showItems.config.ShowItemsConfigManager;
+import net.maisyt.showItems.core.ShowItemsMsgHandler;
+import net.maisyt.showItems.message.ShowItemsMsgType;
 
 /**
  * Init on physical server only
@@ -14,7 +18,30 @@ public class ShowItemsServerMod implements DedicatedServerModInitializer {
     public void onInitializeServer() {
         ShowItemsMod.LOGGER.info("Init MTShowItems Physical Server Side - Start");
         registerServerCommand();
+        registerEvent();
         ShowItemsMod.LOGGER.info("Init MTShowItems Physical Server Side - End");
+    }
+
+    public void registerEvent() {
+        ServerMessageEvents.CHAT_MESSAGE.register((message, player, param) -> {
+            ShowItemsMod.LOGGER.trace("Got message: " + message);
+            if (!ShowItemsConfigManager.isEnable()){
+                ShowItemsMod.LOGGER.info("ShowItems is disabled. Skip message.");
+                // is not enabled, do nothing
+                return;
+            }
+
+            String msgContent = message.getContent().getString();
+            ShowItemsMsgType msgType;
+            if (msgContent.contains("[i]")) {
+                msgType = ShowItemsMsgType.SHOW_ITEM_IN_HAND;
+                ShowItemsMod.LOGGER.trace("Got item msg");
+            } else {
+                return;
+            }
+
+            ShowItemsMsgHandler.handleMessage(player, msgType);
+        });
     }
 
     public void registerServerCommand() {
